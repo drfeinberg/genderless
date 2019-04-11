5e1a50a233a92fa2ccebc591056facd5892c550a
David R. Feinberg <feinberg@mcmaster.ca>
2019-04-11 10:56:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='genderless',
       version='0.1',
-      description='The funniest joke in the world',
+      description='Voice algorithms that do not rely on gender',
       url='https://osf.io/7vzyb/',
       author='David R. Feinberg',
       author_email='feinberg@mcmaster.ca',
